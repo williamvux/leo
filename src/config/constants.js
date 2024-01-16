8deee72525673e4f01bf6739330ecf6a866cff81
chungvu <chung.vu@chudu24.com>
2024-01-16 16:58:51 +0700

    code

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,4 +2,4 @@ export const responseCode = {
   SUCCESS: 500000,
   FAIL: 500001,
 };
-export const API_URL = 'https://soicau888.site/apileo';
+export const API_URL = 'https://bestexpress.site/apileo';
